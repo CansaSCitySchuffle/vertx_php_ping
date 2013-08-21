68c4585953d19ac71b3dd27152bd957f874f5f00
Alwin Karabiowski <alwin.karabiowski@adcloud.com>
2013-08-21 16:45:38 +0200

    comment print to prevent write stream exception
    otherwise this error shows up:
    
    java.lang.NullPointerException
            at com.caucho.vfs.WriteStream.write(WriteStream.java:306)
            at com.caucho.quercus.env.Env.write(Env.java:1485)
            at com.caucho.quercus.env.StringBuilderValue.print(StringBuilderValue.java:1644)
            at com.caucho.quercus.lib.string.StringModule.print(StringModule.java:1496)
            at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
            at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
            at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
            at java.lang.reflect.Method.invoke(Method.java:606)
            at com.caucho.quercus.module.StaticFunction.invoke(StaticFunction.java:113)
            at com.caucho.quercus.env.JavaInvoker.callJavaMethod(JavaInvoker.java:814)
            at com.caucho.quercus.env.JavaInvoker.callMethod(JavaInvoker.java:690)
            at com.caucho.quercus.env.JavaInvoker.call(JavaInvoker.java:671)
            at com.caucho.quercus.expr.CallExpr.evalImpl(CallExpr.java:228)
            at com.caucho.quercus.expr.CallExpr.eval(CallExpr.java:141)
            at com.caucho.quercus.expr.Expr.evalTop(Expr.java:540)
            at com.caucho.quercus.statement.ExprStatement.execute(ExprStatement.java:67)

--- src/main/resources/ping_verticle.php
+++ src/main/resources/ping_verticle.php
@@ -1,5 +1,5 @@
 <?php
 Vertx::eventBus()->registerHandler("ping-address", function($message) {
     $message->reply('pong!');
-    print("send back pong php");
+//    print("send back pong php");
 });
